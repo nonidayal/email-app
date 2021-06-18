6eccdc00781cb16df316f1ad98edbe04f7ad0d50
nonidayal <nonidalal32@gmail.com>
2021-06-18 14:47:04 +0000

    change

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,6 +6,19 @@ sap.ui.define([
 	return Controller.extend("sample.emailtest.controller.View1", {
 		onInit: function () {
 
+		},
+		onSendMail: function () {
+			// var that = this;
+			Email.send({
+				Host: "smtp.elasticemail.com",
+				Username: "knonidayal@gmail.com",
+				Password: "B595D35BDA4783A4799D5C224C5A4577DB1B",
+				To: "nonidalal32@gmail.com",
+				From: "knonidayal@gmail.com",
+				Subject: "Selected tab is " +
+					this.getView().byId("idIconTabBarNoIcons").getSelectedKey(),
+				Body: "And this is the body",
+			}.bind(this)).then((message) => alert(message));
 		}
 	});
 });
